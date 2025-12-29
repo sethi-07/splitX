9dc1a702762d0daf5c87bfdb686d274fbf624c68
Abhinandan Sethi <abhinandansethi07@gmail.com>
2025-12-30 01:50:06 +0530

    Auth done

---
+++ components/header.jsx
@@ -0,0 +1,23 @@
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>header</div>
+    <SignedOut>
+      <SignInButton />
+      <SignUpButton>
+      <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+        Sign Up
+      </button>
+      </SignUpButton>
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  </>
+  )
+}
+
+export default Header
